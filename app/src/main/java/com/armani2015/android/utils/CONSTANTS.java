7ebcce751191afdc7143926bcfdfb1821597266c
Armani2015 <kikithilawi@gmail.com>
2016-05-31 11:05:16 +0100

    Unused fields removed

--- app/src/main/java/com/armani2015/android/utils/CONSTANTS.java
+++ app/src/main/java/com/armani2015/android/utils/CONSTANTS.java
@@ -8,15 +8,6 @@ import com.armani2015.android.R;
  */
 public class CONSTANTS {
 
-    public static final String PRODUCTS_DICTIONARY_EN_RESOURCE = "articles_en.txt";
-    public static final String PRODUCTS_DICTIONARY_FR_RESOURCE = "articles_fr.txt";
-    public static final String PRODUCTS_LIST_RESOURCE = "kiakiaItems.xml";
-    public static final String XML_TAG_CATEGORY = "category";
-    public static final String XML_TAG_CATEGORY_NAME = "categoryname";
-    public static final String XML_TAG_PRODUCT = "product";
-    public static final String XML_TAG_PRODUCT_NAME = "productname";
-    public static final int ITEM_COUNT_TAG = 101;
-    public static final int PRODUCT_lIST_ITEM_TAG_KEY = 102;
     public static final int MAX_CELLS_PER_GRID_ROW = 4;
     public static final int[] CELLS_IDS = {
             R.id.cell_1,
